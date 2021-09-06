86f4b92e60032c61207809b688f6ee0263740b7d
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-09-06 12:35:53 +0700

    Tugas 32 Javascript

---
+++ tugas32.js
@@ -0,0 +1,7 @@
+function panggilJS(){
+  console.log(document.getElementById("namadepan"));
+  console.log(document.getElementById("namabelakang"));
+  console.log(document.getElementById("hobi"))
+}
+
+panggilJS()
